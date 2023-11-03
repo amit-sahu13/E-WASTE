0dbe11c85da0eba36c6019ce91537726140bff03
manmohansingh325 <manmohankumar325@gmail.com>
2023-11-03 11:32:41 +0530

    login form added

---
+++ JAVA-SCRIPTS/login.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const signInButton = document.getElementById("signInButton");
+  const signUpButton = document.getElementById("signUpButton");
+  const signInForm = document.querySelector(".form-section.sign-in");
+  const signUpForm = document.querySelector(".form-section.sign-up");
+
+  signInButton.addEventListener("click", function (e) {
+    e.preventDefault(); //default behavior rokne k liye
+    signInForm.style.display = "block";
+    signUpForm.style.display = "none";
+  });
+
+  signUpButton.addEventListener("click", function (e) {
+    e.preventDefault(); //default behavior rokne k liyess
+    signUpForm.style.display = "block";
+    signInForm.style.display = "none";
+  });
+});
