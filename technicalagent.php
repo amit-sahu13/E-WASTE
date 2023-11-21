36fe91edfded813e01b752ecb2a2e5b33cac64d9
Amit Sahu <amitkumarsahu718@gmail.com>
2023-11-21 14:02:09 +0530

    location updated

--- technicalagent.php
+++ technicalagent.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E-Waste | Agent</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="CSS/technicalagentform.css">
     <link rel="stylesheet" href="CSS/header.css">
     <link rel="stylesheet" href="CSS/footer.css">
